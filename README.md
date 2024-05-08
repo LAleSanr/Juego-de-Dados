22376480da1d81b872956232b85e06ef3012f280
LAleSanr <107571025+LAleSanr@users.noreply.github.com>
2024-05-07 23:21:13 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,18 @@
+# my_first_app
+
+A new Flutter project.
+Para este proyecto se duplico el dado para que puedan tener 2 dados y puedan ser randomizados los 2.
+Ademas, se cambio el color dentro del bloque de dice_roller del primer plano para poder diferenciar los 2 dados mostrados y se agrego negritas al texto.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\gerar\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\gerar\\OneDrive\\Documents\\FlutterApp\\flutter-roll_dice-main" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\gerar\\flutter"
+  "PROJECT_DIR=C:\\Users\\gerar\\OneDrive\\Documents\\FlutterApp\\flutter-roll_dice-main"
+  "FLUTTER_ROOT=C:\\Users\\gerar\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\gerar\\OneDrive\\Documents\\FlutterApp\\flutter-roll_dice-main\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\gerar\\OneDrive\\Documents\\FlutterApp\\flutter-roll_dice-main"
+  "FLUTTER_TARGET=C:\\Users\\gerar\\OneDrive\\Documents\\FlutterApp\\flutter-roll_dice-main\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9jNGNkNDhlMTg2NDYwYjMyZDQ0NTg1Y2UzYzEwMzI3MWFiNjc2MzU1Lw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\gerar\\OneDrive\\Documents\\FlutterApp\\flutter-roll_dice-main\\.dart_tool\\package_config.json"
+)
